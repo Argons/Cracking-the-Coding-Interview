aa528835f94bc79a5ce16803a0659e62cfed5209
Sol <SolArgons@gmail.com>
2013-12-06 14:30:30 +0800

    Arrays and Strings.cpp

---
+++ Arrays and Strings.cpp
@@ -0,0 +1,203 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+/// 1.1  time:O(n) space:O(n)
+bool TestUniqueChars(const string &str)
+{
+    int len = str.length();
+    bool ASCIITable[256] = { false };
+    for (int i = 0; i < len; i++) {
+        int index = str[i];
+        if (ASCIITable[index])
+            return false; // duplicate found.
+        ASCIITable[index] = true;
+    }
+    return true;
+}
+
+/// 1.2 -reverse a string
+void ReverseString(char str[])
+{
+    int len = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+        len++;
+    int i = --len;
+    // exchange characters with symmetric index until reach the center of string.
+    while (i - (len-i) > 1) {
+        swap(str[i], str[len-i]);
+        i--;
+    }
+}
+
+#include <bitset>
+/// 1.3 -remove dupilcate characters from string.
+void RemoveDuplicates(char str[])
+{
+    if (str == NULL)
+        return;
+    bitset<256> ASCIITable;
+    ASCIITable.reset();
+
+    int len = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+        len++;
+    int new_len = 1;
+    ASCIITable.set(str[0]);
+    for (int i = 1; i < len; i++) {
+        int idx = str[i];
+        if (!ASCIITable[idx]) {
+            str[new_len++] = str[i];
+            // while removing some components or in need of two sets of index,
+            // traverse along one index inside the if(...){} and the other outside.
+            ASCIITable.set(idx);
+        }
+    }
+    str[new_len] = '\0';
+}
+void test()
+{
+    char str[5][10] = {"abcd", "aaaa", "", "aaabbb", "abababa"};
+    for (int i = 0; i < 5; i++) {
+        cout<<str[i]<<"\t";
+        RemoveDuplicates(str[i]);
+        cout<<str[i]<<endl;
+    }
+}
+
+/// 1.4 -decide whether two strings are anagrams.
+#include <cstdlib>
+int compare(const void *a, const void *b)
+{
+    return *(char*)a - *(char*)b;
+}
+bool TestAnagrams(char a[], char b[])
+{
+    int a_len = 0, b_len = 0;
+    for (int i = 0; a[i] != '\0'; i++) {
+        a_len++;
+    }
+    for (int i = 0; b[i] != '\0'; i++) {
+        b_len++;
+    }
+    if (a_len != b_len)
+        return false;
+    qsort (a, a_len, sizeof(char), compare);
+    qsort (b, b_len, sizeof(char), compare);
+    for (int i = 0; i < a_len; i++) {
+        if (a[i] != b[i])
+            return false;
+    }
+    return true;
+}
+
+/// 1.5 -replace all spaces in a string with '%20'.
+void ReplaceSpaces(char str[])
+{
+    int len = 0, count_spaces = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ')
+            count_spaces++;
+        len++;
+    }
+    int new_len = len + count_spaces * 2;
+    str[new_len] = '\0';
+    // traverse backwards, otherwise the characters unvisited might have been overwritten already.
+    for (int i = len - 1; i >= 0; i--) {
+        if (str[i] == ' ') {
+            str[new_len-1] = '0';
+            str[new_len-2] = '2';
+            str[new_len-3] = '%';
+            new_len -= 3;
+        }
+        else {
+            str[new_len-1] = str[i];
+            new_len--;
+        }
+    }
+}
+
+/// 1.6 -rotate an image in place, which is represented by an N*N matrix, by 90 degrees.
+void RotateMatrix(int **matrix, int n)
+{
+    // rotate by layers, altogether n/2 layers in the matrix.
+    for (int i = 0; i < n / 2; i++) {
+        int start = i;
+        int end = n - 1 - i;
+        for (int j = start; j < end; j++) {
+            int top = matrix[i][j];
+            matrix[i][j] = matrix[n-1-j][i];         // left -> top
+            matrix[n-1-j][i] = matrix[n-1-i][n-1-j]; // bottom -> left
+            matrix[n-1-i][n-1-j] = matrix[j][n-1-i]; // right -> bottom
+            matrix[j][n-1-i] = top;                  // top -> right
+        }
+    }
+}
+
+/// 1.7 -if M[i][j]=0, set its entire row and column to 0.
+void SetZeros(int **matrix, int row_num, int col_num)
+{
+    bool row[row_num];
+    bool col[col_num];
+    for (int i = 0; i < col_num; i++)
+        col[i] = false;
+    for (int i = 0; i < row_num; i++) {
+        row[i] = false;
+        for (int j = 0; j < col_num; j++) {
+            if (matrix[i][j] == 0) {
+                // mark the rows and columns that contain 0.
+                row[i] = true;
+                col[j] = true;
+            }
+        }
+    }
+    for (int i = 0; i < row_num; i++) {
+        for (int j = 0; j < col_num; j++) {
+            if (row[i] == true || col[j] == true) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+}
+
+/// 1.8 -call isSubstring() once to determine rotation of a string.
+bool isSubstring(string substring, string str)
+{
+    if (str.find(substring) == -1)
+        return false;
+    return true;
+}
+bool isRotation(string str1, string str2)
+{
+    if (str1.length() != str2.length())
+        return false;
+    string cat_two_str1 = str1 + str1;
+    return isSubstring(str2, cat_two_str1);
+}
+
+//int main()
+//{
+////    char *str = new char[50];
+////    cin.getline(str, 50);
+////    cout<<str<<endl;
+//    int m, n;
+//    cin>>m>>n;
+//    int **M = new int*[m];
+//    for (int i = 0; i < m; i++) {
+//        M[i] = new int[n];
+//        for (int j = 0; j < n; j++)
+//            cin>>M[i][j];
+//    }
+//    cout<<endl;
+//    SetZeros(M, m, n);
+//    cout<<"After SetZeros:"<<endl;
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n; j++)
+//            cout<<M[i][j]<<" ";
+//        cout<<endl;
+//    }
+//    delete [] M;
+//    return 0;
+//}
---
+++ Linked Lists.cpp
@@ -0,0 +1,249 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+    Node(int d, Node *Next = NULL): data(d), next(Next) {}
+};
+
+class LinkedList {
+    public:
+    Node *head;
+
+    LinkedList(Node *node_head=NULL): head(node_head) {}
+    Node* getHead() { return head; }
+    int length()
+    {
+        int count = 0;
+        Node *current = head;
+        while (current != NULL) {
+            count++;
+            current = current->next;
+        }
+        return count;
+    }
+    void append(int newData)
+    {
+        Node *newNode = new Node(newData);
+        newNode->next = head;
+        head = newNode;
+    }
+    void addToTail(int newData)
+    {
+        if (head == NULL) {
+            head = new Node(newData);
+            return;
+        }
+        Node *newNode = new Node(newData);
+        Node *current = head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+    void deleteNode(int d)
+    {
+        Node *current = head;
+        while (current != NULL) {
+            if (current->next->data == d) {
+                current->next = current->next->next;
+                return;
+            }
+            current = current->next;
+        }
+    }
+    void insertNth(int data, int n)
+    {
+        if (n > length())
+            return;
+        Node *current = head;
+        for (int i = 1; i < n-1; i++) {
+            current = current->next;
+        }
+        Node *temp = new Node(data);
+        temp->next = current->next;
+        current->next = temp;
+    }
+    void printLinkedList()
+    {
+        if (head == NULL)
+            return;
+        cout<<head->data;
+        Node *current = head->next;
+        while (current != NULL) {
+            cout<<" -> "<<current->data;
+            current = current->next;
+        }
+        cout<<endl;
+    }
+    void bubbleSort()
+    {
+        Node *a, *b;
+        for (a = head; a->next != NULL; a = a->next) {
+            for (b = a->next; b != NULL; b = b->next) {
+                if (a->data > b->data) {
+                    swap(a->data, b->data);
+                }
+            }
+        }
+    }
+    void insertionSort()
+    {
+        if (head == NULL)
+            return;
+        Node *a;
+        Node *b;
+        for (a = head->next; a != NULL; a = a->next) {
+            for (b = head; b != a; b = b->next) {
+                if (a->data < b->data)
+                    swap(a->data, b->data);
+            }
+        }
+    }
+
+    void removeDups();
+    Node* findNthToLast(int);
+};
+
+/// 2.1 -Remove duplicates
+#include <set>
+void LinkedList::removeDups()
+{
+    if (head == NULL)
+        return;
+
+    set<int> table;
+    Node *current = head;
+    table.insert(current->data);
+    while (current->next != NULL) {
+        if (table.find(current->next->data) != table.end())
+            current->next = current->next->next;
+        else {
+            table.insert(current->next->data);
+            current = current->next; // if current->next is deleted, pointer current retains.
+        }
+    }
+}
+
+/// 2.2 -Find the Nth to last element
+Node* LinkedList::findNthToLast(int n)
+{
+    Node *ptr1 = head;
+    for (int i = 0; i < n; i++) {
+        if (ptr1 == NULL)
+            return NULL; // if n>length, return NULL
+        ptr1 = ptr1->next;
+    }
+    Node *current = head;
+    for (Node *ptr2 = ptr1; ptr2 != NULL; ptr2 = ptr2->next) {
+        current = current->next;
+    }
+    return current;
+}
+
+/// 2.3 -Delete node n in the middle of linked list given only the access of this node.
+void deleteNode(Node *n)
+{
+    if (n == NULL || n->next == NULL)
+        return;
+    n->data = n->next->data;
+    n->next = n->next->next;
+}
+
+/// 2.4 -Add up two numbers reversely stored in two lists, return the sum as a linked list.
+LinkedList addReversedLists(LinkedList list1, LinkedList list2, int carry = 0)
+{
+    if (list1.head == NULL && list2.head == NULL && carry == 0)
+        return NULL;
+    int data = carry;
+    if (list1.head != NULL) {
+        data += list1.head->data;
+    }
+    if (list2.head != NULL) {
+        data += list2.head->data;
+    }
+    carry = data / 10;
+    LinkedList res = addReversedLists((list1.head == NULL ? NULL : list1.head->next),
+                                      (list2.head == NULL ? NULL : list2.head->next),
+                                      carry);
+    res.addToTail(data % 10);
+    return res;
+}
+
+/// 2.5 -Given a circular linked list, return node at the beginning of the loop.
+bool hasCycle(Node*);
+Node *FindBeginning(LinkedList list)
+{
+    if (!hasCycle(list.head)) {
+        return NULL;
+    }
+
+    Node *a = list.head;
+    Node *b = list.head;
+    while (b->next != NULL) {
+        a = a->next;
+        b = b->next->next;
+        if (a == b) {
+            break;
+        }
+    }
+    if (b->next == NULL) {
+        return NULL;
+    }
+    a = list.head;
+    while (a != b) {
+        a = a->next;
+        b = b->next;
+    }
+    return a;
+}
+bool hasCycle(Node *head) {
+    if (head == NULL || head->next == NULL) {
+        return false;
+    }
+    Node *n1 = head;
+    Node *n2 = head->next;
+    while (n2 != NULL) {
+        if (n1 == n2) {
+            return true; // n1(slower) and n2(faster) will meet if cycle exists.
+        }
+        n1 = n1->next;
+        n2 = n2->next;
+        if (n2 != NULL) {
+            n2 = n2->next;
+        }
+        else {
+            break;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int data;
+    cin>>data;
+    Node *a = new Node(data);
+    LinkedList list1(a);
+
+    for (int i=0; i<3; i++) {
+        cin>>data;
+        list1.addToTail(data);
+    }
+
+    int data2;
+    cin>>data2;
+    Node *b = new Node(data2);
+    LinkedList list2(b);
+
+    for (int i=0; i<5; i++) {
+        cin>>data2;
+        list2.addToTail(data2);
+    }
+
+    list1.printLinkedList();
+    list2.printLinkedList();
+
+    return 0;
+}
